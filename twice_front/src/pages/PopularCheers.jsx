bbf06b79e12cf4fbbbc887c85d727a1e2cb5405b
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-20 16:43:29 +0900

    #1 feat/ 인기 응원함 페이지 만들기

---
+++ twice_front/src/pages/PopularCheers.jsx
@@ -0,0 +1,13 @@
+import StoryInput from '../components/StoryInput';
+
+
+const PopularCheers = () => {
+  return (
+    <div style={{ width:"70vw", height:"100vh", display: "flex", alignContent: "center", flexDirection: "column", padding:"20px 25px", gap:"20px" }}>
+      <div style = {{fontWeight:'900', fontSize:'1.5em'}}>인기 응원함</div>
+      <StoryInput />
+    </div> 
+  );
+}
+
+export default PopularCheers;
