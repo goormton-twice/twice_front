bac0c78679e52886721b3e9a4ae61d611bb888bc
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-25 21:22:42 +0900

    응원 컴포넌트 수정

--- twice_front/src/components/StoryInput.jsx
+++ twice_front/src/components/StoryInput.jsx
@@ -1,35 +1,38 @@
+import Bookmark from "./bookmark";
 import Button from "./Button";
 import Likes from "./likes";
 import "./StoryInput.css";
 const StoryInput = (props) => {
-  const { border = "1px solid black", hasTags, hasLikes } = props;
+  const {
+    hasTags = false,
+    hasLikes = false,
+    style = {},
+    hasBookmark = false,
+    children = null
+  } = props;
   return (
-    <div id="outline" style={{ border: border }}>
-      {hasTags && (
-        <div className="div-flex">
-          <Button disabled={true}>HOT</Button>
-          <Button disabled={true}>위로</Button>
+    <div id="outline" style={style}>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}>
+        <div className="div-flex" style={{ gap: "5px", alignItems: "center" }}>
+          <img src="../src/assets/react.svg" />
+          <div>닉네임</div>
+          <div>05.16</div>
         </div>
-      )}
-
-      <div className="div-flex" style={{ marginLeft: "5px" }}>
-        <img src="../src/assets/react.svg" />
-        <div>닉네임</div>
-        <div>05.16</div>
+        {hasTags && <Button disabled>위로</Button>}
       </div>
-      <div
-        className="div-flex"
-        style={{ justifyContent: "space-between", marginLeft: "5px" }}
-      >
-        <div style={{ fontWeight: "bold", fontSize: "1.1em" }}>사연 내용</div>
+
+      <div style={{ fontWeight: "normal", fontSize: "0.9em", marginBottom:"10px" }}>{children}</div>
+      <div style={{ display: "flex", width:"100%", justifyContent: "space-between" }}>
         {hasLikes && (
-          <div className="div-flex">
+          <div className="div-flex" style={{ gap: "5px" }}>
             <div>
               <Likes />
             </div>
+            <div>응원</div>
             <div>3</div>
           </div>
         )}
+        {hasBookmark && <Bookmark />}
       </div>
     </div>
   );
