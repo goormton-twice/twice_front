01df8c99ebb847e3e546481e33e56368affb3e9d
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-25 20:43:04 +0900

    icon component

---
+++ twice_front/src/components/Arrow.jsx
@@ -0,0 +1,6 @@
+
+export default function Arrow(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>{/* Icon from ProIcons by ProCode - https://github.com/ProCode-Software/proicons/blob/main/LICENSE */}<path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m14.25 4.75l-6.19 6.19a1.5 1.5 0 0 0 0 2.12l6.19 6.19"></path></svg>
+  )
+}
