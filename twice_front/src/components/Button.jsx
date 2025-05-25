509686958cd3ce159bf354fac9a6d837e0b62402
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-25 21:22:07 +0900

    버튼 props 추가

--- twice_front/src/components/Button.jsx
+++ twice_front/src/components/Button.jsx
@@ -1,14 +1,25 @@
-const Button = ({ children, onClick, disabled = false, className = '', style = {}}) => { 
+const Button = ({ style, children, ...props }) => {
   return (
     <button
-      onClick={onClick}
-      disabled={disabled}
-      style={{ backgroundColor:'white', color: 'black', border: '1px solid black', padding:'1px 10px', ...style }}
+      style={{
+        backgroundColor: 'white',
+        height: "18px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "12px",
+        color: 'black',
+        border: '1px solid black',
+        margin: "0",
+        padding: '1px 10px',
+        borderRadius: '20px',
+        ...style // 사용자 정의 style 덮어쓰기 가능
+      }}
+      {...props} // 나머지 props (onClick 등)
     >
       {children}
     </button>
   );
-
-}
+};
 
 export default Button;
