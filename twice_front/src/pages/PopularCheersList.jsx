d5821a8243555e573b78d4db6124938889eefc5b
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-25 21:32:45 +0900

    인기응원함 css 수정

--- twice_front/src/pages/PopularCheersList.jsx
+++ twice_front/src/pages/PopularCheersList.jsx
@@ -1,14 +1,31 @@
-import StoryInput from '../components/StoryInput';
-
+import StoryInput from "../components/StoryInput";
 
 const PopularCheersList = () => {
-
   return (
-    <div style={{ width:"70vw", height:"100vh", display: "flex", alignContent: "center", flexDirection: "column", padding:"20px 25px", gap:"20px" }}>
-      <div style = {{fontWeight:'900', fontSize:'1.5em'}}>인기 응원함</div>
-      <StoryInput hasTags ={true} hasLikes={true} />
-    </div> 
+    <div style={{ padding: "20px" }}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignContent: "center",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <div style={{ fontWeight: "900", fontSize: "1.5em" }}>인기 응원함</div>
+        <StoryInput
+          hasTags={true}
+          hasLikes={true}
+          style={{
+            borderWidth: "0 0 1px 0",
+            borderStyle: "solid",
+            borderColor: "#B3B2B2",
+          }}
+        >사연 내용</StoryInput>
+      </div>
+    </div>
   );
-}
+};
 
 export default PopularCheersList;
