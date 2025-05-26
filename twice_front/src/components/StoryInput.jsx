8b082fc3a20597a7bbfb89d78fb00e4ad389567a
HyeEun_Lee <blackhaze1024@gmail.com>
2025-05-26 15:40:49 +0900

    css 수정

--- twice_front/src/components/StoryInput.jsx
+++ twice_front/src/components/StoryInput.jsx
@@ -13,7 +13,7 @@ const StoryInput = (props) => {
   return (
     <div id="outline" style={style}>
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}>
-        <div className="div-flex" style={{ gap: "5px", alignItems: "center" }}>
+        <div className="div-flex" style={{ gap: "5px", alignItems: "center", fontSize: "14px" }}>
           <img src="../src/assets/react.svg" />
           <div>닉네임</div>
           <div>05.16</div>
@@ -24,9 +24,9 @@ const StoryInput = (props) => {
       <div style={{ fontWeight: "normal", fontSize: "0.9em", marginBottom:"10px" }}>{children}</div>
       <div style={{ display: "flex", width:"100%", justifyContent: "space-between" }}>
         {hasLikes && (
-          <div className="div-flex" style={{ gap: "5px" }}>
+          <div className="div-flex" style={{ gap: "5px", fontSize: "14px"  }}>
             <div>
-              <Likes />
+              <Likes style={{width:"25px"}} />
             </div>
             <div>응원</div>
             <div>3</div>
