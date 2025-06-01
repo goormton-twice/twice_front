ee044de8b80fc394dca215c2e957a0eae57b83b1
HyeEun_Lee <blackhaze1024@gmail.com>
2025-06-01 17:20:16 +0900

    feat: 나의 사연 보는 페이지 만들기

---
+++ twice_front/src/pages/MyCheer.jsx
@@ -0,0 +1,52 @@
+import Arrow from "../components/Arrow";
+import Footer from "../components/Footer";
+import StoryInput from "../components/StoryInput";
+const MyCheer = () => {
+  const cheers = [
+    {
+      id: 1,
+      hasLikes: true,
+      tag: "위로",
+      content:
+        "요즘은 아무리 열심히 해도 결과가 잘 안 보여서 지치고 있어요.공부는 하고 있는데, 내가 잘하고 있는 건지 잘 모르겠어요.정말 이 길이 내가 원하는 길인지 점점 헷갈리기도 하고요.",
+      style: {
+        borderWidth: "0 0 1px 0",
+        borderStyle: "solid",
+        borderColor: "#B3B2B2",
+      },
+      hasBookmark: true,
+    },
+  ];
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <div style={{ padding: "25px 20px" }}>
+        <div
+          style={{
+            display: "flex",
+            fontWeight: "900",
+            fontSize: "1.5em",
+            alignItems: "center",
+            gap: "5px",
+            marginBottom: "5px",
+          }}
+        >
+          <Arrow style={{ fontSize: "1.2em" }} />
+          <div>나의 사연 쓰기</div>
+        </div>
+        {cheers.map((cheer) => (
+          <StoryInput
+            key={cheer.id}
+            hasLikes={cheer.hasLikes}
+            style={cheer.style}
+            hasBookmark={cheer.hasBookmark}
+          >
+            {cheer.content}
+          </StoryInput>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MyCheer;
