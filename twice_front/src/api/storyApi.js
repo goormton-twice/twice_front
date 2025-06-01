2af7e9bdb487337e0eb7ee98a9a82a02e25bbd3c
Damin <kimdm5112@gmail.com>
2025-06-01 21:15:32 +0900

    feat: 로그인, 로그아웃 api 연결

---
+++ twice_front/src/api/storyApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://api.cheer-up.net/api',
+  withCredentials: true,
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const getRandomStories = async (size = 3) => {
+  const response = await API.get(`/stories/random?size=${size}`);
+  return response.data.data;
+};
