a890f43cd188945c9cccc3b438bea579372cf997
HyeEun_Lee <blackhaze1024@gmail.com>
2025-06-01 17:19:25 +0900

    feat: 사연 카테고리 만들기

--- twice_front/src/components/Category.jsx
+++ twice_front/src/components/Category.jsx
@@ -1,6 +1,6 @@
 const Category = (props) => {
   return (
-    <button style={{ width:"maxContent", height: "maxContent", border:"1px solid #878787", display:"flex", padding:"5px 5px", borderRadius:"30%" }} >
+    <button id={props.id} style={{  border:"1.5px solid #878787", backgroundColor:"white", display:"flex", gap:"10px", padding:"11px", borderRadius:"18px",alignItems:"center", fontWeight:"500", height:"38px", margin:"10px 0",...props.style }} onClick={props.onClick}>
       <div>{props.detail}</div>
       <div>#{props.tag}</div>
     </button>
