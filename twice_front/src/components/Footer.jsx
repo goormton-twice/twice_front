074fc4c5ea238d9cadf8346da632051f0157c63c
HyeEun_Lee <blackhaze1024@gmail.com>
2025-06-01 17:18:48 +0900

    feat: footer 만들기

--- twice_front/src/components/Footer.jsx
+++ twice_front/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import HomeImg from "./HomeImg";
 import Likes from "./likes";
 import Cheer from "./Cheer";
 import PersonalCheerImg from "./PersonalCheerImg";
-
+import PopularCheerImg from './PopularCheerImg';
+import "./Footer.css"; 
 const Footer = () => {
   const [clicked, setClicked] = useState();
   return (
@@ -12,32 +13,34 @@ const Footer = () => {
         display: "flex",
         justifyContent: "space-around",
         alignItems: "center",
-        width: "100vw",
+        width: "100%",
         height: "70px",
-        background: "red",
+        background: "",
         position: "fixed",
         bottom: "0",
+        padding:"10px 5px",
+        boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.1)",
       }}
     >
-      <button>
-        <HomeImg />
-        <div>홈</div>
+      <button className='footer-btn' onClick={() => setClicked("home")}>
+        <HomeImg className='icons' stroke={clicked === "home" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)"} />
+        <div style={{ color: clicked === "home" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)" }}>홈</div>
       </button>
-      <button>
-        <Likes />
-        <div>무조건 응원함</div>
+      <button className='footer-btn' onClick={() => setClicked("likes")}>
+        <Likes className='icons' stroke={clicked === "likes"? "rgba(152, 108, 233, 1)":"rgba(206, 206, 206, 1)"}  />
+        <div style={{ color: clicked === "likes" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)" }}>무조건 응원함</div>
       </button>
-      <button>
-        <Cheer />
-        <div>사연 쓰기</div>
+      <button className='footer-btn' onClick={() => setClicked("cheer")}>
+        <Cheer className='icons' stroke={clicked === "cheer"? "rgba(152, 108, 233, 1)":"rgba(206, 206, 206, 1)"}/>
+        <div style={{ color: clicked === "cheer" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)" }}>사연 쓰기</div>
       </button>
-      <button>
-        <PopularCheerImg />
-        <div>인기 응원함</div>
+      <button className='footer-btn' onClick={() => setClicked("popularCheer")}>
+        <PopularCheerImg className='icons' stroke={clicked === "popularCheer"? "rgba(152, 108, 233, 1)":"rgba(206, 206, 206, 1)"}/>
+        <div style={{ color: clicked === "popularCheer" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)" }}>인기 응원함</div>
       </button>
-      <button>
-        <PersonalCheerImg />
-        <div>개인 응원함</div>
+      <button className='footer-btn' onClick={() => setClicked("personalCheer")}>
+        <PersonalCheerImg className='icons' stroke={clicked ==="personalCheer"? "rgba(152, 108, 233, 1)":"rgba(206, 206, 206, 1)"}/>
+        <div style={{ color: clicked === "personalCheer" ? "rgba(152, 108, 233, 1)" : "rgba(206, 206, 206, 1)" }}>개인 응원함</div>
       </button>
     </div>
   );
