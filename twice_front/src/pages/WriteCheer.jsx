1274db1510583453687a19de06f32d4260059f11
HyeEun_Lee <blackhaze1024@gmail.com>
2025-06-01 17:19:50 +0900

    feat: 사연 쓰기 페이지 만들기

--- twice_front/src/pages/WriteCheer.jsx
+++ twice_front/src/pages/WriteCheer.jsx
@@ -1,20 +1,121 @@
-import Category from '../components/Category';
+import { useRef, useState } from 'react';
+import Button from "../components/Button";
+import Category from "../components/Category";
+import Footer from "../components/Footer";
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  &::placeholder {
+    color: rgba(206, 206, 206, 1);
+    font-weight: 500;
+    font-family: "Pretendard";
+  }
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 38px;
+  border: 1.7px solid rgba(152, 108, 233, 1);
+  border-radius: 18px;
+  padding: 11px;
+  box-shadow: 0px 4px 4px rgba(81, 80, 80, 0.25);
+`;
 
 const WriteCheer = () => {
   const Categories = [
-    { id: 1, detail: '내 편 좀 들어줘', tag:"다정한" },
-    { id: 2, detail: '위로가 필요해', tag:"따뜻한" },
-    { id: 3, detail: '잘하고 있다는 말이 듣고 싶어', tag:"든든한" },
+    { id: 1, detail: "내 편 좀 들어줘", tag: "다정한", onclick: () => {} },
+    { id: 2, detail: "위로가 필요해", tag: "따뜻한", onclick: () => {} },
+    {
+      id: 3,
+      detail: "잘하고 있다는 말이 듣고 싶어",
+      tag: "든든한",
+      onclick: () => {},
+    },
   ];
+  const [selectedId, setSelectedId] = useState(null);
+  const [isEnrolled, setIsEnrolled] = useState(false);
+  const handleEnroll = () => {
+  setIsEnrolled(true);
+};
+  const handleClick = (id) => {
+  if (selectedId === id) {
+    setSelectedId(null); // 이미 선택된 거면 해제
+  } else {
+    setSelectedId(id); // 선택
+  }
+};
   return (
     <div>
-      <div>닉네임 님의 <span>사연</span>을 들려주세요</div>
-      <div>어떤 응원을 위한 사연인가요?</div>
-      {Categories.map((category) =>{
-        <Category props = {category} />
-      })}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "40px 30px",
+        }}
+      >
+        <div style={{ fontSize: "24px", fontWeight: "600" }}>
+          닉네임 님의 <span style={{ color: "" }}>사연</span>을 <br />
+          들려주세요
+        </div>
+        <div style={{ height: "30px" }} />
+        <form
+          style={ !selectedId ? {display: "none"} : {
+            display: "flex",
+            flexDirection: "column",
+            gap: "5px",
+            marginBottom: "30px",
+          }}
+        >
+          <label style={{ marginBottom: "10px", fontWeight: "500" }}>
+            당신의 사연은 무엇인가요?
+          </label>
+          <TextArea placeholder="나의 사연 남기기" />
+          <Button
+            style={{
+              position: "fixed",
+              bottom: "220px",
+              right: "120px",
+              width: "95px",
+              height: "35px",
+              borderRadius: "25px",
+              backgroundColor: "rgba(152, 108, 233, 1)",
+              border: "0",
+              color: "white",
+              gap:"5px",
+              padding: "10px 15px",
+            }}
+            onClick={handleEnroll}
+          >
+            {isEnrolled && <div>✓</div>}
+            등록하기
+          </Button>
+        </form>
+        <div style={{ marginBottom: "10px", fontWeight: "500" }}>
+          어떤 응원을 위한 사연인가요?
+        </div>
+        <div>
+          {Categories.map((item) => {
+            return (
+              <Category
+                key={item.id}
+                detail={item.detail}
+                tag={item.tag}
+                onClick={() => handleClick(item.id)}
+                style={
+                  selectedId === item.id
+                    ? {
+                        border: "1.5px solid rgba(152, 108, 233, 1)",
+                        color: "rgba(152, 108, 233, 1)",
+                      }
+                    : {}
+                }
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default WriteCheer;
