13a4e00dfbdafc4149404d12ad388020fcef7ab5
Damin <kimdm5112@gmail.com>
2025-06-02 19:45:28 +0900

    feat: 사연함 api 연결

--- twice_front/src/pages/SignIn.jsx
+++ twice_front/src/pages/SignIn.jsx
@@ -29,56 +29,61 @@ export default function LoginPage() {
     }
   };
 
-  return (
+return (
     <div className="login-container">
-      <div className="content-wrapper">
-        <div className="logo">
-          <img src="/signinlogo.svg" alt="logo" />
-        </div>
-        <p className="subtitle">당신의 하루에 작은 <br />응원을 담아요</p>
+        <div className="content-wrapper">
+            <div className="logo">
+                <img src="/signinlogo.svg" alt="logo" />
+            </div>
+            <p className="subtitle">당신의 하루에 작은 <br />응원을 담아요</p>
 
-        <form className="login-form" onSubmit={handleLogin}>
-          <input
-            type="email"
-            placeholder="이메일"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input-field"
-          />
-          <div className="input-password-wrapper">
-            <input
-              type="password"
-              placeholder="비밀번호"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"
-            />
-            <span className="eye-icon">👁️</span>
-          </div>
-          <button type="submit" className="login-button">로그인</button>
-        </form>
+            <form className="login-form" onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="이메일"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="input-field"
+                />
+                <div className="input-password-wrapper">
+                    <input
+                        type="password"
+                        placeholder="비밀번호"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="input-field"
+                    />
+                    <span className="eye-icon">👁️</span>
+                </div>
+                <button type="submit" className="login-button">로그인</button>
+            </form>
 
-        <div className="help-links">
-          <span>비밀번호 찾기</span>
-          <span>|</span>
-          <span>아이디 찾기</span>
-          <span>|</span>
-          <span>회원가입</span>
-        </div>
+            <div className="help-links">
+                <span>비밀번호 찾기</span>
+                <span>|</span>
+                <span>아이디 찾기</span>
+                <span>|</span>
+                <span 
+                    className="signup-link" 
+                    onClick={() => navigate('/signup')}
+                >
+                    회원가입
+                </span>
+            </div>
 
-        <div className="sns-login-section">
-          <div className="divider">
-            <span className="line" />
-            <span className="label">간편 로그인</span>
-            <span className="line" />
-          </div>
-          <div className="sns-buttons">
-            <NaverLoginButton />
-            <GoogleLoginButton />
-            <KakaoLoginButton />
-          </div>
+            <div className="sns-login-section">
+                <div className="divider">
+                    <span className="line" />
+                    <span className="label">간편 로그인</span>
+                    <span className="line" />
+                </div>
+                <div className="sns-buttons">
+                    <NaverLoginButton />
+                    <GoogleLoginButton />
+                    <KakaoLoginButton />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  );
+);
 }
