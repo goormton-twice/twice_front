2aec2d06c9f9eabd197018e2cc9f043054996f98
Damin <kimdm5112@gmail.com>
2025-06-03 09:45:45 +0900

    my page 수정

--- twice_front/src/pages/MyPage.jsx
+++ twice_front/src/pages/MyPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getUserInfo } from '../api/userApi';
-import './MyPage.css';
+import './MySupportPage.css';
 import Arrow from '../components/Arrow';
 
 export default function MyPage() {
